91c75959b2233e4c2ffca51d8fccd2fd96f7de5b
Mike Wallio <rawrizaur@gmail.com>
2020-06-02 22:00:03 -0400

    Run code format

--- winwal.psm1
+++ winwal.psm1
@@ -5,12 +5,12 @@
 function Set-Wallpaper {
     param(
         # Path to image to set as background, if not set current wallpaper is used
-        [Parameter(Mandatory=$true)][string]$Image
+        [Parameter(Mandatory = $true)][string]$Image
     )
 
-# Trigger update of wallpaper
-# modified from https://www.joseespitia.com/2017/09/15/set-wallpaper-powershell-function/
-Add-Type -TypeDefinition @"
+    # Trigger update of wallpaper
+    # modified from https://www.joseespitia.com/2017/09/15/set-wallpaper-powershell-function/
+    Add-Type -TypeDefinition @"
 using System;
 using System.Runtime.InteropServices;
 
@@ -21,16 +21,15 @@ public class PInvoke
 }
 "@
 
-# Setting the wallpaper requires an absolute path, so pass image into resolve-path
-[PInvoke]::SystemParametersInfo(0x0014, 0, $($Image | Resolve-Path), 0x0003) -eq 1
+    # Setting the wallpaper requires an absolute path, so pass image into resolve-path
+    [PInvoke]::SystemParametersInfo(0x0014, 0, $($Image | Resolve-Path), 0x0003) -eq 1
 }
 
 <#
 .DESCRIPTION
     Gets the location of the module.
 #>
-function Get-ScriptDirectory
-{
+function Get-ScriptDirectory {
     Split-Path $script:MyInvocation.MyCommand.Path
 }
 
@@ -50,50 +49,50 @@ function Add-WalTemplates {
 }
 
 function Update-WalCommandPrompt {
-$scriptDir = Get-ScriptDirectory
-$colorToolDir = "$scriptDir/colortool"
-$colorTool = "$colorToolDir/ColorTool.exe"
-$schemesDir = "$colorToolDir/schemes/"
+    $scriptDir = Get-ScriptDirectory
+    $colorToolDir = "$scriptDir/colortool"
+    $colorTool = "$colorToolDir/ColorTool.exe"
+    $schemesDir = "$colorToolDir/schemes/"
 
-# Install color tool if needed
-if (!(Test-Path -Path $colorTool)) {
-    $colorToolZip = "$scriptDir/colortool.zip"
-    Invoke-WebRequest -Uri "https://github.com/microsoft/terminal/releases/download/1904.29002/ColorTool.zip" -OutFile $colorToolZip
-    Expand-Archive -Path $colorToolZip -DestinationPath $colorToolDir
-    Remove-Item -Path $colorToolZip
-}
+    # Install color tool if needed
+    if (!(Test-Path -Path $colorTool)) {
+        $colorToolZip = "$scriptDir/colortool.zip"
+        Invoke-WebRequest -Uri "https://github.com/microsoft/terminal/releases/download/1904.29002/ColorTool.zip" -OutFile $colorToolZip
+        Expand-Archive -Path $colorToolZip -DestinationPath $colorToolDir
+        Remove-Item -Path $colorToolZip
+    }
 
-Copy-Item -Path "$HOME/.cache/wal/wal-prompt.ini" -Destination "$schemesDir/wal.ini"
-& $colorTool -d wal.ini
+    Copy-Item -Path "$HOME/.cache/wal/wal-prompt.ini" -Destination "$schemesDir/wal.ini"
+    & $colorTool -d wal.ini
 
 }
 
 function Update-WalTerminal {
 
-$terminalProfile = "$HOME/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
+    $terminalProfile = "$HOME/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
 
-if (!(Test-Path $terminalProfile)) {
-    # We'll probably fail out if we don't return early
-    return
-}
+    if (!(Test-Path $terminalProfile)) {
+        # We'll probably fail out if we don't return early
+        return
+    }
 
-# Load existing profile
-$configData = (Get-Content -Path $terminalProfile | ConvertFrom-Json)[0]
+    # Load existing profile
+    $configData = (Get-Content -Path $terminalProfile | ConvertFrom-Json)[0]
 
-# Create a new list to store schemes
-$schemes = New-Object Collections.Generic.List[Object]
+    # Create a new list to store schemes
+    $schemes = New-Object Collections.Generic.List[Object]
 
-$configData.schemes | Where-Object { $_.name -ne "wal" } | ForEach-Object { $schemes.Add($_) }
-$schemes.Add($(Get-Content "$HOME/.cache/wal/windows-terminal.json" | ConvertFrom-Json))
+    $configData.schemes | Where-Object { $_.name -ne "wal" } | ForEach-Object { $schemes.Add($_) }
+    $schemes.Add($(Get-Content "$HOME/.cache/wal/windows-terminal.json" | ConvertFrom-Json))
 
-# Update color schemes
-$configData.schemes = $schemes
+    # Update color schemes
+    $configData.schemes = $schemes
 
-# Set default theme as wal
-$configData.profiles.defaults | Add-Member -MemberType NoteProperty -Name colorScheme -Value 'wal' -Force
+    # Set default theme as wal
+    $configData.profiles.defaults | Add-Member -MemberType NoteProperty -Name colorScheme -Value 'wal' -Force
 
-# Write config to disk
-$configData | ConvertTo-Json -Depth 32 | Set-Content -Path $terminalProfile
+    # Write config to disk
+    $configData | ConvertTo-Json -Depth 32 | Set-Content -Path $terminalProfile
 }
 
 <#
@@ -106,36 +105,36 @@ function Update-WalTheme {
         [string]$Image
     )
 
-$img = (Get-ItemProperty -Path 'HKCU:/Control Panel/Desktop' -Name Wallpaper).Wallpaper
-if ($Image) {
-    $img = $Image
-}
+    $img = (Get-ItemProperty -Path 'HKCU:/Control Panel/Desktop' -Name Wallpaper).Wallpaper
+    if ($Image) {
+        $img = $Image
+    }
 
-# Add our templates to wal configuration
-Add-WalTemplates
+    # Add our templates to wal configuration
+    Add-WalTemplates
 
-$tempImg = "$env:TEMP/$(Split-Path $img -leaf)"
+    $tempImg = "$env:TEMP/$(Split-Path $img -leaf)"
 
-# Use temp location, default backgrounds are in a write protected directory
-Copy-Item -Path $img -Destination $tempImg
+    # Use temp location, default backgrounds are in a write protected directory
+    Copy-Item -Path $img -Destination $tempImg
 
-# Invoke wal with colorthief backend and don't set the wallpaper (wal will fail)
-$light = $(Get-ItemProperty -Path 'HKCU:/SOFTWARE/Microsoft/Windows/CurrentVersion/Themes/Personalize' -Name AppsUseLightTheme).AppsUseLightTheme
-if ($light -gt 0) {
-    wal -n -e -l -s -t -i $tempImg --backend colorthief
-}
-else {
-    wal -n -e -s -t -i $tempImg --backend colorthief
-}
+    # Invoke wal with colorthief backend and don't set the wallpaper (wal will fail)
+    $light = $(Get-ItemProperty -Path 'HKCU:/SOFTWARE/Microsoft/Windows/CurrentVersion/Themes/Personalize' -Name AppsUseLightTheme).AppsUseLightTheme
+    if ($light -gt 0) {
+        wal -n -e -l -s -t -i $tempImg --backend colorthief
+    }
+    else {
+        wal -n -e -s -t -i $tempImg --backend colorthief
+    }
 
-# Set the wallpaper
-if ($Image) {
-    Set-Wallpaper -Image $Image
-}
+    # Set the wallpaper
+    if ($Image) {
+        Set-Wallpaper -Image $Image
+    }
 
-# Update Windows Terminal
-Update-WalTerminal
+    # Update Windows Terminal
+    Update-WalTerminal
 
-# Update prompt defaults
-Update-WalCommandPrompt
+    # Update prompt defaults
+    Update-WalCommandPrompt
 }
