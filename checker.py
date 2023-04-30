c5df3171b68399c95ed1de406982d1263f1e7581
Mike Wallio <michael.wallio@outlook.com>
2023-04-30 16:52:19 -0400

    Move validate set checkers to python

--- checker.py
+++ checker.py
@@ -1,7 +1,15 @@
 import sys
 from importlib import util
 import json
+import shutil
 
 modules: list[str] = json.load(sys.stdin)
 available = [module for module in modules if util.find_spec(module)]
+
+if shutil.which("magick"):
+    available.append("wal")
+
+if shutil.which("schemer2"):
+    available.append("schemer2")
+
 print(json.dumps(available))
